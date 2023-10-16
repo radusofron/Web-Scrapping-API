6c74fc40eb17b1e42435e79d71419fc285528911
radusofron <radu.sofron169@gmail.com>
2023-10-16 17:31:55 +0300

    title and short description scraped + error 404 handled

--- webapp/src/main.py
+++ webapp/src/main.py
@@ -1,12 +1,12 @@
 from flask import Flask, request, render_template, flash
-from scrapper import getData
+from scrapper import get_data
 
 
 app = Flask(__name__, template_folder="../templates", static_folder="../static")
 app.config["SECRET_KEY"] = "e0bRS0hCmelkXu2pBbw35IfGNDf860rCN5eh8r8f_NxiAWNiFN0ApB2ep5dhB_ZajHqCUbYmfT9Y8bXIE4_1RQ"
 
 
-def validate_URL(url):
+def validate_URL(url: str):
     """Function validates URL inserted
     """
     valid_url = "https://wsa-test.vercel.app/"
@@ -32,11 +32,16 @@ def scrape():
             data = "Invalid URL"
         else:
             # Scrape website
-            data = getData(url)
+            data = get_data(url)
 
-        return render_template('index.html', data=data)
+        return render_template("index.html", data=data)
     # GET method
-    return render_template('index.html')
+    return render_template("index.html")
+
+# Handle error 404
+@app.errorhandler(404)
+def error_404(error):
+    return render_template("error_404.html")
 
 
 if __name__ == '__main__':
--- webapp/src/scrapper.py
+++ webapp/src/scrapper.py
@@ -1,26 +1,51 @@
 import requests as req
+from selenium import webdriver
 from bs4 import BeautifulSoup
+import json
 
 
-def getData(url):
+def get_title_and_short_description(div):
+    """Function scrapes title and short description for every div
+    """
+    object = dict()
+
+    # Convert BS4 element to string
+    div=str(div)
+    
+    # Create scrapper
+    soup = BeautifulSoup(div, 'html.parser')
+
+    # Scrape title
+    # First div -> to select the current div
+    object["title"] = soup.find("div").find("div", recursive=False).find_all("div", recursive=False)[1].find("a").text
+    object["short description"] = soup.find("div").find("div", recursive=False).find_all("div", recursive=False)[1].find_all("div", recursive=False)[1].text
+    return object
+
+
+def get_data(url: str):
     """Function scrapes website content
     """
-    response = req.get(url)
-    print(response)
-    try:
-        response = req.get(url)
-        response.raise_for_status()  # Check for HTTP errors
+    # Set the type of browser => headless browser (to not open a browser window)
+    options = webdriver.ChromeOptions()
+    options.add_argument("--headless")
+    # Navigate to the webpage using Chrome
+    driver = webdriver.Chrome(options=options)
+    driver.get(url)
+    # Get page source
+    page_source = driver.page_source
+    # Close browser
+    driver.quit()
+    
+    # Scrape the webapge
+    soup = BeautifulSoup(page_source, 'html.parser')
 
-        webpage = BeautifulSoup(response.content, 'html.parser')
-        title = webpage.title
-
-        # # Extract title and short description (change selectors as needed)
-        # title = soup.find('title').get_text()
-        # description = soup.find('meta', attrs={'name': 'description'})['content']
-
-        # return {"title": title, "description": description}
-
-        return title
-
-    except Exception as e:
-        return {"error": str(e)}
+    # Scrape level 6 divs
+    level_6_divs = soup.find("div").find("main").find("div").find("div").find_all("div", recursive=False)[1].find_all("div", recursive=False)
+    # Scrape divs content
+    data = []
+    for div in level_6_divs:
+        data.append(get_title_and_short_description(div))
+    
+    # Convert into JSON format
+    json_data = json.dumps(data, indent=4)
+    return json_data
